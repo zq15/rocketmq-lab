dd1c9321c84dba6dbb5b68e2c353bd22997e565c
zds <1529219484@qq.com>
2024-11-14 14:06:14 +0800

    批量发送消息

---
+++ src/main/java/org/zds/mq/rocketmq/batch/Producer.java
@@ -0,0 +1,31 @@
+package org.zds.mq.rocketmq.batch;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * 批量，但注意大小不可超过 1MiB
+ */
+public class Producer {
+    public static void main(String[] args) throws Exception {
+        // 实例化消息生产者Producer
+        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+        // 设置NameServer的地址
+        producer.setNamesrvAddr("192.168.139.10:9876");
+        // 启动Producer实例
+        producer.start();
+        producer.setRetryTimesWhenSendAsyncFailed(0);
+        List<Message> messages = new ArrayList<>();
+        messages.add(new Message("BatchTopic", "Tag", "OrderID001", "Hello world 0".getBytes()));
+        messages.add(new Message("BatchTopic", "Tag", "OrderID002", "Hello world 1".getBytes()));
+        messages.add(new Message("BatchTopic", "Tag", "OrderID003", "Hello world 2".getBytes()));
+
+        producer.send(messages);
+        producer.shutdown();
+    }
+}
